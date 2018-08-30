9acc73dc2511d90013447908572c72ec9f9d3ac5
Robyn Dalgleish <hello@robyndalgleish.com>
2018-08-30 12:19:21 -0400

    icing

--- src/Containers/Landing.js
+++ src/Containers/Landing.js
@@ -1,15 +1,26 @@
 
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { bounce } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+
+const bounceAnimation = keyframes`${bounce}`;
+
+const BouncyDiv = styled.div`
+  animation: 2s ${bounceAnimation};
+`;
+
 
 class Landing extends Component {
 
   render() {
     return(
-      <div className='plumContainer'>
-        <img className='plum' alt='photo of woman saying Hey!' src='../assets/images/plum.svg'/>
-        <div>
-          <h1>plum</h1>
+      <div className='plumWhooContainer'>
+        <BouncyDiv>
+          <img className='plumWhoo' alt='photo of woman saying Hey!' src='../assets/images/plumWhoo.svg'/>
+        </BouncyDiv>
+        <div className='plumContainer'>
+          <img className='plum' alt='plum' src='../assets/images/plum.svg'/>
         </div>
         <h2>A loyalty system for all your hard work.</h2>
         <div className='plumCta'>
